b6e90881f2b9008da7ff6b84b59cdb871f93ef1e
Hagin <Hagin@hagincubes@gmail.com>
2020-03-02 15:32:30 -0500

    implemented output of matrix as well as mutation algorithm.

--- src/com/hagincodes/snakeai/NeuralNet.java
+++ src/com/hagincodes/snakeai/NeuralNet.java
@@ -43,5 +43,46 @@ public class NeuralNet {
 	}
 	
 	
+	//calculate the output values of feeding forward through the NN
+	public float[] output(float[] inputArr) {
+		
+		
+		//convert array to matrix
+		
+		Matrix inputs = woh.singleColumnMatrixFromArray(inputArr);
+		
+		//add bias
+		Matrix inputBias = inputs.addBias();
+		
+		//calculate based on the output too...........
+		
+		
+		//apply layer of one weight to the inputs
+		Matrix hiddenInputs = whi.dot(inputBias);
+		
+		
+		//pass through activation function(sigmoid)		
+		Matrix hiddenOutputs = hiddenInputs.activate();
+		
+		//add bias
+		Matrix hiddenOutputsBias = hiddenOutputs.addBias();
+		
+		//apply layer to two weights
+		Matrix hiddenInputs2 = whh.dot(hiddenOutputsBias);
+		Matrix hiddenOutputs2 = hiddenInputs2.activate();
+		Matrix hiddenOutputBias2 = hiddenOutputs2.addBias();
+		
+		//apply level 3 weights
+		Matrix outputInputs = woh.dot(hiddenOutputBias2);
+		//pass through sigmoid function
+		
+		Matrix outputs = outputInputs.activate();
+		
+		return outputs.toArray();
+		
+		
+	}
+	
+	
 	
 }
